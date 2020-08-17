8bdc07bb61c9a02fd15cb3a6003a4475a288a516
notblisy <50887637+notblisy@users.noreply.github.com>
2020-08-17 19:52:27 -0400

    Code to cause profiles to work with button press.

--- Simple_Controllers_-_Build-a-Box_-_Version_1.0.ino
+++ Simple_Controllers_-_Build-a-Box_-_Version_1.0.ino
@@ -1,3 +1,4 @@
+
 #include "Nintendo.h"
 /* This code uses the Nicohood Library
  * Use this code at your own risk
@@ -13,19 +14,31 @@ Gamecube_Data_t d = defaultGamecubeData;   //Structure for data to be sent to co
 //This is needed but you don't need a controller on pin 7
 CGamecubeController GamecubeController1(7);
 
+// Ints that will change when the SWITCH push button is pushed and increases a counter for profile switching.
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+
+
+
 //This is the pinout of the controller.  Can be changed to your liking.  I may have mixed up some of the tilt pins but you can change that physically in your build or through the code.  Just do test runs along the way.
 const int A = 22;
 const int B = 24;
 const int X = 26;
-const int Y = 28;
-const int Z = 30;
+const int Y = 30;
+const int Z = 35;
 const int START = 31;
 
-const int R = 34;
-const int L = 35;
-const int RLIGHT = 36;
+ int R = 34; // This is not a const int anymore because it needs to be overwitten for profile switching.
+ int L = 36;
+ int RLIGHT = 28; // This is not a const int anymore because it needs to be overwitten for profile switching.
 //This is the value of analog shielding 74 is lightest possible on gamecube.  It varies from gamecube to dolphin no idea why.
-const int RLIGHTv = 74;
+ int RLIGHTv = 74; // This is not a const int anymore because it needs to be overwitten for profile switching.
+
+int LLIGHT = 28;
+int LLIGHTv = 74;
+
 
 const int LEFT = 38;
 const int RIGHT = 39;
@@ -44,8 +57,8 @@ const int X1v = 27;
 const int X2v = 55;
 const int X3v = 73;
 
-const int Y1v = 27;
-const int Y2v = 53;
+      int Y1v = 27; // This is not a const int anymore because it needs to be overwitten for profile switching.
+      int Y2v = 53; // This is not a const int anymore because it needs to be overwitten for profile switching.
 const int Y3v = 74;
 
 const int CLEFT = 48;
@@ -69,7 +82,7 @@ void setup()
   pinMode(R, INPUT_PULLUP);
   pinMode(L, INPUT_PULLUP);
   pinMode(RLIGHT, INPUT_PULLUP);
-  
+  pinMode(LLIGHT, INPUT_PULLUP);
   pinMode(LEFT, INPUT_PULLUP);
   pinMode(RIGHT, INPUT_PULLUP);
   pinMode(UP, INPUT_PULLUP);
@@ -89,6 +102,8 @@ void setup()
 
   //This is needed to run the code.
   GamecubeController1.read();
+
+ 
 }
 
 void loop()
@@ -104,6 +119,7 @@ void loop()
   int pinR = 0;
   int pinL = 0;
   int pinRLIGHT = 0;
+  int pinLLIGHT = 0;
 
   int pinLEFT = 0;
   int pinRIGHT = 0;
@@ -182,13 +198,19 @@ void loop()
   if (digitalRead(CDOWN) == LOW && digitalRead(CUP) == HIGH)pinyAxisC = 0;
 
   if (digitalRead(A) == LOW)pinA = 1;
+  
+   
   if (digitalRead(B) == LOW)pinB = 1;
+  
   if (digitalRead(X) == LOW)pinX = 1;
   if (digitalRead(Y) == LOW)pinY = 1;
   if (digitalRead(Z) == LOW)pinZ = 1;
   if (digitalRead(START) == LOW)pinSTART = 1;
+    
+  
   //This is for analog shield
   if (digitalRead(RLIGHT) == LOW)pinRLIGHT = RLIGHTv;
+  if (digitalRead(LLIGHT) == LOW)pinLLIGHT = LLIGHTv;
   //This is for digital shield
   if (digitalRead(R) == LOW)pinR = 1;
   if (digitalRead(L) == LOW)pinL = 1;
@@ -200,15 +222,89 @@ void loop()
   d.report.ddown = 0;
   d.report.dleft = 0;
     
-  //NEW WHEN SWITCH/BUTTON ON PIN 12 IS PRESSED/ACTIVATED SWAPS X1,X2,Y1,Y2 TO DPAD 
-  if (pinSWITCH == 1){
-    if(digitalRead(X1) == LOW)d.report.dleft = 1;
+
+
+// This code causes the values of Y1v, Y2v, RLIGHT, R, and RLIGHTv to switch based on a buttonstate counter.
+// Essentially, if SWITCH is pushed, it increments buttonPushCounter by 1, and if it's a multiple of 3 it switches to
+// Y1v and Y2v have appropriate angles for Project M shield drop and utilt. If it's a multiple of 4 it switches
+// Y1v and Y2v for uptilt purposes in Smash Ult, then it switches R and RLIGHTs position on the pinout, because
+// Smash ultimate cannot read hard press shield. Then, it sets RLIGHTv to 255, which is the hardest light shield press
+// So you can get a normal shield. More profiles can be added by doing more multiples. 
+ buttonState = digitalRead(SWITCH);
+
+ if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      buttonPushCounter++;
+    } 
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+
+  //This is the Project M profile I have set up.
+  if (buttonPushCounter % 3 == 0) {
+    
+  Y2v = 70;
+  Y1v = 53;
+   //This is the smash Ultimate profile I have setup.
+  } else if(buttonPushCounter % 4 == 0){
+    
+  Y2v = 70;
+  Y1v = 43;
+  RLIGHT = 34;
+  R = 28;
+  RLIGHTv = 255;
+  LLIGHT = 36;
+  L = 28;
+  LLIGHTv = 255;
+
+  }//This is the monster hunter / steam generic controller. It can't read the hard press and I have enabled to dpad since most modern games require it.
+   else if(buttonPushCounter % 5 == 0){
+    
+  Y2v = 70;
+  Y1v = 43;
+  RLIGHT = 34;
+  R = 28;
+  RLIGHTv = 255;
+  LLIGHT = 36;
+  L = 28;
+  LLIGHTv = 255;
+  
+   if(digitalRead(Y2) == LOW)d.report.dleft = 1;
     if(digitalRead(X2) == LOW)d.report.ddown = 1;
     if(digitalRead(Y1) == LOW)d.report.dup = 1;
-    if(digitalRead(Y2) == LOW)d.report.dright = 1;
-  }
+    if(digitalRead(X1) == LOW)d.report.dright = 1;
   
+    } else {
+      //This is the melee profile, it is both the default and if you're on a mod that's not 3/4/5 then it'll be melee. 
+      Y2v = 53;
+      Y1v = 27;
+      RLIGHT = 28;
+      R = 34;
+      RLIGHTv = 74;
+      LLIGHT = 28;
+      LLIGHTv = 74;
+      L = 36;
+      }
+// These are commented out because it conflicts with how I have set up the profile switcher.
+// Considering making the taunt button the light shield button and making modifier button cause light shield. 
+// if (digitalRead(SWITCH) == LOW)pinSWITCH = 1;
 
+//  d.report.dup = 0;
+ // d.report.dright = 0;
+ // d.report.ddown = 0;
+ // d.report.dleft = 0;
+    
+  //NEW WHEN SWITCH/BUTTON ON PIN 12 IS PRESSED/ACTIVATED SWAPS X1,X2,Y1,Y2 TO DPAD 
+  //if (pinSWITCH == 1){
+  // if(digitalRead(X1) == LOW)d.report.dleft = 1;
+   // if(digitalRead(X2) == LOW)d.report.ddown = 1;
+    //if(digitalRead(Y1) == LOW)d.report.dup = 1;
+   // if(digitalRead(Y2) == LOW)d.report.dright = 1;
+ // }
 
   //reports data
   d.report.a = pinA;
@@ -220,12 +316,11 @@ void loop()
   d.report.r = pinR;
   d.report.l = pinL;
   d.report.left = pinRLIGHT;
+  d.report.right = pinLLIGHT;
   d.report.xAxis = pinxAxis;
   d.report.yAxis = pinyAxis;
   d.report.cxAxis = pinxAxisC;
   d.report.cyAxis = pinyAxisC;
   //sends the complied data to console when console polls for the input
   GamecubeConsole.write(d);
-
 }
-
